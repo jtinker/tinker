8efae09ae2dbeb5dcc6387401660e5df4b5c992d
Jonathan Tinker <jtinker@incomm.com>
2022-08-28 09:42:21 -0600

    Initial commit

---
+++ src/test/java/com/tinker/tinkerweb/TinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinker.tinkerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
